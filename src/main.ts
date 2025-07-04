830555c4bb8beb73b9ba6299b65d2edc28455cfe
Pavel Havelka <havelka.pvl@seznam.cz>
2025-07-04 21:06:20 +0200

    countdown start

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,42 @@ import { menuBgMusic, hoverSoundEffect } from "./index";
 menuBgMusic();
 hoverSoundEffect();
 
+
+
+
+// přechod stránky po kliknutí na play
 const playBTN = document.getElementById("playbtn") as HTMLAnchorElement | null;
 
 if (playBTN) {
-  playBTN.addEventListener("click", (e: MouseEvent) => {
-    e.preventDefault();
-    document.body.classList.toggle("animate-page-slide-down");
-    
+    playBTN.addEventListener("click", (e: MouseEvent) => {
+        e.preventDefault();
+        document.body.classList.toggle("animate-page-slide-down");
+        
+        setTimeout(() => {
+            window.location.href = "./src/play.html";
+        }, 500);
+    });
+};
+
+
+// odpočet
+const modalStartGame = document.getElementById("modalStartGame") as HTMLDialogElement | null;
+
+if(modalStartGame){
+    let countdown = 3; // Initial value for start game.
+
     setTimeout(() => {
-      window.location.href = "./src/play.html";
+        modalStartGame.textContent = countdown.toString();
+        modalStartGame.showModal();
+
+        const intervalID = setInterval(() => {
+            countdown--;
+            modalStartGame.textContent = countdown.toString();
+    
+            if(countdown < 0){
+                clearInterval(intervalID);
+                modalStartGame.close();
+            };
+        }, 1000);
     }, 500);
-  });
 };
