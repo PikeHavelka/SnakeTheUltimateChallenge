4fbbc77f2d30d792338259c00a06c1d6bd080a74
Pavel Havelka <havelka.pvl@seznam.cz>
2025-07-04 16:43:23 +0200

    added slide when click on play button

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,16 @@ import { menuBgMusic, hoverSoundEffect } from "./index";
 
 menuBgMusic();
 hoverSoundEffect();
+
+const playBTN = document.getElementById("playbtn") as HTMLAnchorElement | null;
+
+if (playBTN) {
+  playBTN.addEventListener("click", (e: MouseEvent) => {
+    e.preventDefault();
+    document.body.classList.toggle("animate-page-slide-down");
+    
+    setTimeout(() => {
+      window.location.href = "./src/play.html";
+    }, 500);
+  });
+};
